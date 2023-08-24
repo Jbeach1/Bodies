83df5d9fd8ef0dd0261428ba47803c18bcce3406
Wilson Beach <joshbeach99@gmail.com>
2023-08-24 10:50:39 -0400

    Initial stab of using groups

--- Bodies/Bodies/Data/BlazorChatSampleHub.cs
+++ Bodies/Bodies/Data/BlazorChatSampleHub.cs
@@ -6,10 +6,18 @@ namespace BlazorChat
     {
         public const string HubUrl = "/chat";
 
+        
+        public async Task Broadcast(string username, string message, string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("Broadcast", username, message);
+        }
+        
+        /*
         public async Task Broadcast(string username, string message)
         {
             await Clients.All.SendAsync("Broadcast", username, message);
         }
+        */
 
         public override Task OnConnectedAsync()
         {
@@ -21,6 +29,11 @@ namespace BlazorChat
         {
             Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
             await base.OnDisconnectedAsync(e);
+        }
+
+        public async Task AddGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
         }
     }
 }
